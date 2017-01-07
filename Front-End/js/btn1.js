f454663083a31e76686bfe4657783c7c3923108b
Jeisson Sanchez <jeisson_96@hotmail.com>
2017-01-07 16:22:16 -0500

    Correccion basica

--- Front-End/js/btn1.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $("#btn1").click(function(){
-        var div = $("#btn1");  
-        div.animate({left: '50px'}, "slow");
-        div.animate({fontSize: '2em'}, "slow");
-    });
-});
